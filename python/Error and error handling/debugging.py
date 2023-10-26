fa1ec57c39a34dcc9247e7cef473b4a511f72410
kaancodess <kaancodes@gmail.com>
2023-10-26 15:14:46 +0300

    debugging and learning pdb library

---
+++ python/Error and error handling/debugging.py
@@ -0,0 +1,24 @@
+# import pdb
+
+# one = "one"
+# two = "two"
+# pdb.set_trace()
+
+# result = one + two 
+
+# three = "three"
+
+# three += result
+
+# print(result)
+
+def add_numbers(a,b,c):
+    import pdb; pdb.set_trace()
+    return a+b+c
+
+add_numbers(1,2,3)
+
+# l => list
+# p => print
+# n => next line
+# c => continue
