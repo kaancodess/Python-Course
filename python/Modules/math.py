e76548cb443a7bb83f117277dbd311c7904ab56a
kaancodess <kaancodes@gmail.com>
2023-10-31 20:25:29 +0200

    math module

---
+++ python/Modules/math.py
@@ -0,0 +1,17 @@
+# import math
+
+# value = dir(math) # getting all the math library functions 
+
+# value = math.sqrt(49) # getting square root of 49
+# value = math.factorial(5) # getting the 5!
+# value = math.floor(5.9) # floor function rounds down 
+# value = math.ceil(5.9) # floor function rounds up
+
+
+################################# 
+# from math import *
+from math import factorial,sqrt
+value = factorial(5)
+value = int(sqrt(25)) # turning float value to int 
+
+print(value)
