aea0b72c1fcd8e99be9dfa89e1892ca1a6aaee1a
kaancodess <kaancodes@gmail.com>
2023-11-14 19:41:45 +0200

    Datetime Module

--- python/Modules/dmodule.py
+++ python/Modules/dmodule.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timedelta
 
 result = dir(datetime) # getting all the datetime functions
 
@@ -15,6 +16,31 @@ result = datetime.strftime(now,'%A') # getting one information at current time Y
 # A > getting day information as a string
 # B > getting  month information as a string
 result = datetime.strftime(now,'%Y %B %A') # getting year , month and day as a string
-print(result)
 
-# 31 31
+
+# t = " 14 November 2023"
+# day, month , year = t.split()
+# print(day)
+# print(month)
+# print(year)
+
+t = "14 November 2023 hour 18:06:53"
+result = datetime.strptime(t, '%d %B %Y hour %H:%M:%S') 
+result = result.year
+
+birthday = datetime(2003,2,19,12,30,10)
+
+result = datetime.timestamp(birthday) # second
+result = datetime.fromtimestamp(result) # second to datetime
+
+result = now - birthday # timedelta
+
+# result = result.days
+
+# result = result.seconds
+result = result.microseconds
+
+result = now + timedelta(days = 10) # adding 10 days
+
+
+print(result)
